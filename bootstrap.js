de7a30df2b8a3c67df9e1c72af096d360f78c09c
Aloso <Aloso@users.noreply.github.com>
2021-05-10 16:48:22 +0000

    deploy: 833e71825f6d0ac215659044d5c0602eaae6cdaa

--- bootstrap.js
+++ bootstrap.js
@@ -168,7 +168,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/unidoc_playground_bg.wasm":"9ba25e9ea73d37e0c063"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/unidoc_playground_bg.wasm":"db214da322e6339f4af2"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
